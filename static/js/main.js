f1e2f4e884f4363c7911546c8945809b9d98eeb2
Jeff McGee <JeffAMcGee@gmail.com>
2013-03-24 17:14:50 -0500

    working client side knockout exmaple

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,12 @@
 
+// Here's my data model
+var ViewModel = function(first, last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();
+    }, this);
+};
+
+
+ko.applyBindings(new ViewModel("Planet", "Earth"));
