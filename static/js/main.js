6bd689a97a010b3d6d64c106cd42231810501b72
Jeff McGee <JeffAMcGee@gmail.com>
2013-03-25 12:00:01 -0500

    two space tabs everywhere

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
 
 // Here's my data model
 var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
-    this.fullName = ko.computed(function() {
-        return this.firstName() + " " + this.lastName();
-    }, this);
+  this.firstName = ko.observable(first);
+  this.lastName = ko.observable(last);
+  this.fullName = ko.computed(function() {
+    return this.firstName() + " " + this.lastName();
+  }, this);
 };
 
 
